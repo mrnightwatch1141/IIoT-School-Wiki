65a4c58f4e2b5bb1fcf14b743a2241b4312bc186
mr.nightwatch <angelo.de.florio.cisco@gmail.com>
2026-09-11 11:48:58 +0200

    Aggiornato smart-tank.ino
    Aggiunta una spiegazione (spero esaustiva) del codice

--- projects/smart-tank/smart-tank.ino
+++ projects/smart-tank/smart-tank.ino
@@ -2,8 +2,28 @@
 Angelo De Florio
 IIoT School Wiki
 
-smart-tank.ino
+==== smart-tank.ino ====
+Programma principale dell'Arduino UNO R2 WiFi.
+Il prototipo Arduino monta un totale di 5 componenti su di una Breadboard:
+- LED RGB (KY-016):
+  Indicherà lo stato del recipiente in base al livello dell'acqua
+  (es. LEVEL:50, LEVEL:25, LEVEL:100);
+- Ricevitore IR (KY-005):
+  Se riceve un impulso IR mostrerà la temperatura che rileverà il sensore
+  della temperatura montato su Arduino. Se la temperatura è < di 30 allora
+  il LED sarà BLU altrimenti ROSSO;
+- Bottone (KY-004):
+  Se premuto mostrerà la temperatura che rileverà il sensore
+  della temperatura montato su Arduino;
+- Sensore della temperatura (KY-001):
+  Rileva la temperatura in base alla posizione della scheda Arduino.
+- Buzzer Passivo (KY-006):
+  Componente privo di generatore interno che necessita un'onda quadra
+  per emettere una frequenza. Impiegato nel segnalare il livello dell'acqua
+  nel recipiente.
 */
+
+// Librerie
 #include <DallasTemperature.h>
 #include <OneWire.h>
 #include <IRremote.hpp>
@@ -24,8 +44,16 @@ const int BUTTON_PIN = 13;
 // IR
 const int IR_RECEIVE_PIN = 7;
 
+/*
+setup()
+Funzione che esegure una sola volta del codice.
+In questo caso inizializza i componenti ed esegue un test
+dei LED e del Buzzer per segnalare l'accensione.
+*/
 void setup() {
   // put your setup code here, to run once:
+
+  // Inizializzazione Seriale e componenti
   Serial.begin(9600);
   sensors.begin();
   pinMode(BUZZER_PIN , OUTPUT);
@@ -59,6 +87,12 @@ void setup() {
   delay(500);
 }
 
+/*
+checkTemp()
+Funzione che rileva la temperatura dal sensore e
+abilita il LED RGB di BLU o ROSSO in base al
+valore rilevato
+*/
 void checkTemp() {
   sensors.requestTemperatures();
   float temp = sensors.getTempCByIndex(0);
@@ -100,15 +134,30 @@ void checkTemp() {
   Serial.println(temp);
 }
 
+/*
+loop()
+Funzione che ripete continuamente una serie di istruzioni.
+Rimane sempre in ascolto per eventuali input provenienti dal
+sensore IR, bottone o dalla porta seriale
+*/
 void loop() {
   // put your main code here, to run repeatedly:
 
+  // Leggi lo stato del bottone
   int buttonState = digitalRead(BUTTON_PIN);
+  // Se è stato premuto...
   if (buttonState == LOW) {
-    checkTemp();
+    checkTemp(); // controllo la temperatura rilevata
   }
 
+  // Se il ricevitore decodifica...
   if (IrReceiver.decode()) {
+    /*
+    Restituisce le informazioni sull'impulso ricevuto:
+    - protocollo (String);
+    - indirizzo  (esadecimale);
+    - codice     (esadecimale).
+    */
     Serial.print("Protocollo: ");
     Serial.println(getProtocolString(IrReceiver.decodedIRData.protocol));
     Serial.print("Indirizzo: 0x");
@@ -118,23 +167,31 @@ void loop() {
     Serial.print("Codice completo: 0x");
     Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
     Serial.println();
+    // Controllo da sistemare in base al telecomando
     if (IrReceiver.decodedIRData.decodedRawData == 0xC) {
       checkTemp();
     }
     IrReceiver.resume();
   }
 
-
+  /*
+  Ricezione della stringa dal server Node-RED:
+  il server Node-RED riceve i dati formattati in JSON dal Gateway
+  e in base al campo distance invia una stringa LEVEL che l'Arduino
+  interpreterà.
+  */
   String input;
   int level;
   if (Serial.available()) {
+    // Leggo l'input fino all'andata a capo
     input = Serial.readStringUntil('\n');
     input.trim();
 
     if (input.startsWith("LEVEL:")) {
+      // Prendo solo le cifre
       level = input.substring(6).toInt();
 
-      // Normale
+      // Livello normale: compreso tra 10 e 90
       if (level > 10 && level < 90) {
         digitalWrite(RED_LED, LOW);
         digitalWrite(GREEN_LED, HIGH);
@@ -148,7 +205,8 @@ void loop() {
         delay(250);
         noTone(BUZZER_PIN);
       }
-      // Trabocco
+        
+      // Trabocco: livello maggiore o uguale a 100
       else if (input >= 100) {
         digitalWrite(RED_LED, LOW);
         digitalWrite(GREEN_LED, LOW);
@@ -157,7 +215,8 @@ void loop() {
         delay(1000);
         noTone(BUZZER_PIN);
       }
-      // Insufficiente
+        
+      // Insufficiente: livello minore di 10
       else {
         digitalWrite(RED_LED, HIGH);
         digitalWrite(GREEN_LED, LOW);
